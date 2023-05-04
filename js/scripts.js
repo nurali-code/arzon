84f7af677b8dd806a37d1b0200048e21d6744229
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-05-04 23:10:55 +0500

    menu, catalog

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ $(document).ready(function () {
     })
 
     /*---------------------------------------------------end*/
+    $('.menu__next').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    })
+
+    /*---------------------------------------------------end*/
     $('.posting-catalog__link').on('click', function () {
         $(this).addClass('active')
         var catalogTab = $(this).data('catalog-tab');
@@ -142,5 +148,49 @@ $(document).ready(function () {
         e.preventDefault()
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top, }, 500,)
     })
+    /*---------------------------------------------------end*/
+
+    /*---------------------------------------------------end*/
+    function hideModals() {
+        $('.modal').fadeOut();
+        $('.modal, body, [data-modal]').removeClass('active');
+    };
+
+    $(function () {
+        function showModal(id) {
+            if ($(id).hasClass('active')) {
+                $(id).fadeOut(300)
+                $(id).removeClass('active');
+                $('body').removeClass('active');
+            } else {
+                $(id).addClass('active')
+                $('body').addClass('active');
+                $(id).fadeIn(300);
+            }
+        }
+        $('[data-modal]').on('click', function (e) {
+            e.preventDefault();
+            $(this).toggleClass('active')
+            showModal('#' + $(this).attr("data-modal"));
+        });
+
+        $('.modal-close, .menu-catalog__close').on('click', () => {
+            hideModals();
+        });
+
+        $(document).on('click', function (e) {
+            if (!(
+                ($(e.target).parents('.modal-content').length) ||
+                ($(e.target).parents('.nav').length) ||
+                ($(e.target).parents('.btn').length) ||
+                ($(e.target).hasClass('nav')) ||
+                ($(e.target).hasClass('btn-menu')) ||
+                ($(e.target).hasClass('btn')) ||
+                ($(e.target).hasClass('modal-content'))
+            )) {
+                hideModals();
+            }
+        });
+    });
 
 });
