dc2dec70fd25e3dbae40b172d44a7b1758078ee4
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-08-03 16:05:11 +0500

    modal for ad_categories

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -57,6 +57,50 @@ $(document).ready(function () {
         }
     });
 
+    /*---------------------------------------------------end*/
+    function hideModals() {
+        $('.modal').fadeOut();
+        $('.modal, body, [data-modal]').removeClass('active');
+    };
+
+    $(function () {
+        function showModal(id) {
+            if ($(id).hasClass('active')) {
+                $(id).fadeOut(300)
+                $(id).removeClass('active');
+                $('body').removeClass('active');
+            } else {
+                $(id).addClass('active')
+                $('body').addClass('active');
+                $(id).fadeIn(300);
+            }
+        }
+        $('[data-modal]').on('click', function (e) {
+            e.preventDefault();
+            hideModals();
+            $(this).toggleClass('active')
+            showModal('#' + $(this).attr("data-modal"));
+        });
+
+        $('.modal-cancel').on('click', () => {
+            hideModals();
+        });
+
+        $(document).on('click', function (e) {
+            if (!(
+                ($(e.target).parents('.modal-content').length) ||
+                ($(e.target).parents('.nav').length) ||
+                ($(e.target).parents('.btn').length) ||
+                ($(e.target).hasClass('nav')) ||
+                ($(e.target).hasClass('btn-menu')) ||
+                ($(e.target).hasClass('btn')) ||
+                ($(e.target).hasClass('modal-content'))
+            )) {
+                hideModals();
+            }
+        });
+    });
+
 
 
 
